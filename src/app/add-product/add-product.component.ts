ac7affbfcbebe417e1952dad5fb6fb76ab981834
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-28 16:02:31 +0530

    read values

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
+  title=""
+  image=""
+  description=""
+  rate=""
+  seller=""
+  quantity=""
+
+
+  readValues=()=>
+  {
+    let data:any={"title":this.title,"image":this.image,"description":this.description,"rate":this.rate,"seller":this.seller,"quantity":this.quantity}
+    console.log(data)
+  }
+
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
